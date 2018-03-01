cccd3b6c61ecdb0af900371971ae0b432f199df3
Sergio De Santiago <checomigue_sm97@hotmail.com>
2018-03-01 08:52:47 -0600

    declaracion de algunas varibales en js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
-var a = 5;
+var a = 5; // Local al bloque
 console.log(a);
 console.log(a+3);
 console.log("hola");
+
+let b = 3; // Local a la funcion
+x = 42; // Global
+const B = 3; // constante con alcance local a la funcion
+
+array 0 [
+  1,
+  2,
+  3
+];
+
+objeto = {
+  atr1: 'cadena',
+  atr2: 5
+};
